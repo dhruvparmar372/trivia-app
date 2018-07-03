688dc768a7227b2bda7e27b969f1cdaa869dbffa
Dhruv Parmar <dhruvparmar@gmail.com>
2018-07-03 10:19:02 +0530

    Add react-native-config

--- source/containers/app/index.js
+++ source/containers/app/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import Config from "react-native-config";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -12,7 +13,9 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>
+          {Config.GREETING} Welcome to React Native!
+        </Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
